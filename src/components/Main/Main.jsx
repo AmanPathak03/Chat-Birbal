41ab135ffabbec9d76252717eeeb48f6bf36ecb8
Aman Pathak <pathakamaniaf2003@gmail.com>
2025-01-15 18:20:42 +0530

    Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../Assets/assets.js';
 import { Context } from '../../context/ContextProvider.jsx';
 import Sidebar from '../Sidebar/Sidebar.jsx';
 import handleVoiceInput from '../../config/handleVoiceInput.js';
-import { processImage } from './imageProcessing';
+import { processImage } from '../../config/imageProcessing';
 
 const Main = () => {
     const { onSent, recentPrompt, showResult, loading, resultData, setInput, input, conversationHistory } = useContext(Context);
